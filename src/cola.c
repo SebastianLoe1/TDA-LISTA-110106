4561805d4c58d7c1be15675e75e5f77d2e8539d3
Bakalin0 <sebasloe@gmail.com>
2023-09-28 17:19:50 -0300

    Cola y pila implementado con lista pero muchos errores

--- src/cola.c
+++ src/cola.c
@@ -105,3 +105,73 @@ void cola_destruir(cola_t *cola)
 
     free(cola);
 }
+
+
+/*
+struct _cola_t {
+    lista_t *lista;
+};
+
+cola_t *cola_crear()
+{
+    cola_t *cola = malloc(sizeof(cola_t));
+    if (!cola) {
+        return NULL;
+    }
+    cola->lista = lista_crear();
+    if (!cola->lista) {
+        free(cola);
+        return NULL;
+    }
+    return cola;
+}
+
+cola_t *cola_encolar(cola_t *cola, void *elemento)
+{
+    if (!cola) {
+        return NULL;
+    }
+    lista_insertar(cola->lista, elemento);
+    return cola;
+}
+
+void *cola_desencolar(cola_t *cola)
+{
+    if (!cola || lista_vacia(cola->lista)) {
+        return NULL;
+    }
+    return lista_quitar(cola->lista);
+}
+
+void *cola_frente(cola_t *cola)
+{
+    if (!cola || lista_vacia(cola->lista)) {
+        return NULL;
+    }
+    return lista_primero(cola->lista);
+}
+
+size_t cola_tamanio(cola_t *cola)
+{
+    if (!cola) {
+        return 0;
+    }
+    return lista_tamanio(cola->lista);
+}
+
+bool cola_vacia(cola_t *cola)
+{
+    if (!cola) {
+        return true;
+    }
+    return lista_vacia(cola->lista);
+}
+
+void cola_destruir(cola_t *cola)
+{
+    if (!cola) {
+        return;
+    }
+    lista_destruir(cola->lista);
+    free(cola);
+}*/
--- src/pila.c
+++ src/pila.c
@@ -1,4 +1,5 @@
 #include "pila.h"
+#include "lista.h"
 
 typedef struct nodo {
     void *elemento;
@@ -91,3 +92,73 @@ void pila_destruir(pila_t *pila)
 
     free(pila);
 }
+
+
+/*struct _pila_t {
+    lista_t *lista;
+};
+
+pila_t *pila_crear()
+{
+    pila_t *pila = malloc(sizeof(pila_t));
+    if (!pila) {
+        return NULL;
+    }
+    pila->lista = lista_crear();
+    if (!pila->lista) {
+        free(pila);
+        return NULL;
+    }
+    return pila;
+}
+
+pila_t *pila_apilar(pila_t *pila, void *elemento)
+{
+    if (!pila) {
+        return NULL;
+    }
+    lista_insertar(pila->lista, elemento);
+    return pila;
+}
+
+void *pila_desapilar(pila_t *pila)
+{
+    if (!pila || lista_vacia(pila->lista)) {
+        return NULL;
+    }
+    return lista_quitar(pila->lista);
+}
+
+void *pila_tope(pila_t *pila)
+{
+    if (!pila || lista_vacia(pila->lista)) {
+        return NULL;
+    }
+    size_t tamanio = lista_tamanio(pila->lista);
+    return lista_elemento_en_posicion(pila->lista, tamanio - 1);
+}
+
+size_t pila_tamanio(pila_t *pila)
+{
+    if (!pila) {
+        return 0;
+    }
+    return lista_tamanio(pila->lista);
+}
+
+bool pila_vacia(pila_t *pila)
+{
+    if (!pila) {
+        return true;
+    }
+    return lista_vacia(pila->lista);
+}
+
+void pila_destruir(pila_t *pila)
+{
+    if (!pila) {
+        return;
+    }
+    lista_destruir(pila->lista);
+    free(pila);
+}*/
